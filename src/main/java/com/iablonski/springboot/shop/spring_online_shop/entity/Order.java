7616570d978aff851d7e0e56726507daff36fe39
Alexander <alex.iablonski@gmail.com>
2023-04-08 22:37:41 +0300

    Commit 1.9

--- src/main/java/com/iablonski/springboot/shop/spring_online_shop/entity/Order.java
+++ src/main/java/com/iablonski/springboot/shop/spring_online_shop/entity/Order.java
@@ -2,7 +2,10 @@ package com.iablonski.springboot.shop.spring_online_shop.entity;
 
 import jakarta.persistence.*;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -10,8 +13,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
--- src/main/java/com/iablonski/springboot/shop/spring_online_shop/entity/OrderDetails.java
+++ src/main/java/com/iablonski/springboot/shop/spring_online_shop/entity/OrderDetails.java
@@ -2,10 +2,12 @@ package com.iablonski.springboot.shop.spring_online_shop.entity;
 
 import jakarta.persistence.*;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
